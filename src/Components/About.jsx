b026cd8370ce30fb718f7035e25c7ba73fe01870
Talha Javed <talhadevstudio30@gmail.com>
2026-02-25 08:08:45 -0800

    added app icon

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <>
-      <div className="text-4xl mt-11 text-center">
+      <div className="text-4xl mt-11 text-center ">
         Have fun using this app with Talha Javed 🚀😄✨
       </div>
     </>
